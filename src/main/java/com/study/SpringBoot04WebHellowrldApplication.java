fd1f632edca454dbc6d4c551d80e8b7a01772adf
wangxingang <wangxingang0610@sina.com>
2019-02-08 21:32:05 +0800

    Initial commit

---
+++ src/main/java/com/study/SpringBoot04WebHellowrldApplication.java
@@ -0,0 +1,14 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04WebHellowrldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot04WebHellowrldApplication.class, args);
+    }
+
+}
+
